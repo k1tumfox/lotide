61c19afeb769a0c68945e29bbb8edfba635de78f
Tenyson Kwon <tenysonkwon@gmail.com>
2020-07-20 23:40:37 +0000

    outputs new array minus head element

--- head.js
+++ head.js
@@ -5,7 +5,7 @@
 const head = function(array) {
   return array[0];
 };
-console.log(head([]));
+//console.log(head([]));
 /* assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([5]), 5);
