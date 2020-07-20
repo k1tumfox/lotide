8e7113928c4bb249ff0379459b72191e48738801
Tenyson Kwon <tenysonkwon@gmail.com>
2020-07-20 22:41:53 +0000

    actual vs expected comparison

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,11 @@
 const assertEqual = function(actual, expected) {
-  let notice;
-  actual === expected ? notice = console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`) : notice = console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  actual === expected ? console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
 };
 
 // TEST CODE
-/* assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
-
+/*
 assertEqual("tomato", "ketchup");
 assertEqual("lhl", "lhl");
 assertEqual(1, 4); */
