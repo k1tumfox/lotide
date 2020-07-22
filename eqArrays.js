e8b18cca55429121f569cd48c09614bd1b80f98b
Tenyson Kwon <tenysonkwon@gmail.com>
2020-07-22 18:18:30 +0000

    compares arrays successfully

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  return actual === expected; //console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = function(array1, array2) {
+  let matchIndicator = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return matchIndicator;//true or false
+}
+
+//console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+//console.log(assertEqual(1, 1));
+
+//console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => should PASS
+
+/* console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false */
