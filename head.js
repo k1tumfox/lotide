83d222cda0bd9fa242b6cae4e00050d00a86b3a7
Tenyson Kwon <tenysonkwon@gmail.com>
2020-08-05 19:32:43 +0000

    refactored 'head' & 'assertEqual' to export/require

--- head.js
+++ head.js
@@ -1,12 +1,8 @@
-/* const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-}; */
-//should return first element, catch undefined arrays
+const assertEqual = require('./assertEqual');
+
 const head = function(array) {
   return array[0];
 };
-//console.log(head([]));
-/* assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), 5); */
+// console.log(head([]));
+
+module.exports = head;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual');
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+/*
+assertEqual("tomato", "ketchup");
+assertEqual("lhl", "lhl");
+assertEqual(1, 4); */
+
